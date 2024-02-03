1c62ab9a72a36b4d86f0cd415a797f9bbce21132
Patrick Zenhäusern <patrick.zenhaeusern@digitecgalaxus.ch>
2024-02-03 20:23:46 +0100

    add firebase rules

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { firestoreDb } from "./firebase.ts";
 import { AuthenticationButtons } from "./authenticationButtons.tsx";
+import { useFireBaseAuthentication } from "./useFireBaseAuthentication.ts";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const { authenticatedUser } = useFireBaseAuthentication();
   return (
     <>
       <div>
@@ -36,11 +37,18 @@ function App() {
   async function handleClick() {
     setCount((count) => count + 1);
     try {
-      const querySnapshot = await getDocs(collection(firestoreDb, "animals"));
+      const querySnapshot = await getDocs(
+        collection(firestoreDb, `users/${authenticatedUser?.uid}/animals`),
+      );
 
       querySnapshot.forEach((doc) => {
         console.log(`${doc.id}`, doc.data());
       });
+
+        // await addDoc(collection(firestoreDb, `users/${authenticatedUser?.uid}/animals`), {
+        //     type: "snake",
+        //     name: "Venice"
+        // });
     } catch (e) {
       console.error("Error adding document: ", e);
     }
