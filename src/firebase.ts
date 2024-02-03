707635623715255db17fa168e9e4db4e2e0f0703
Patrick Zenhäusern <patrick.zenhaeusern@digitecgalaxus.ch>
2024-02-03 14:02:50 +0100

    init

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+﻿import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbYdobDsV0LkZbs2OpoHU0mAXBSehfVEw",
+  authDomain: "snakeeper-c4f9d.firebaseapp.com",
+  projectId: "snakeeper-c4f9d",
+  storageBucket: "snakeeper-c4f9d.appspot.com",
+  messagingSenderId: "987196495591",
+  appId: "1:987196495591:web:4cd13a860ab339e5ff7442",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const firestoreDb = getFirestore(firebaseApp);
+
